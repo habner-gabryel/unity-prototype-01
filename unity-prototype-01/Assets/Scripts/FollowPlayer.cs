e816b800fbcf9027986453fcba72ff894e9122d2
habner-gabryel <habner.gabryel.cc@gmail.com>
2024-07-09 18:15:15 -0300

    1.3.5 commit

--- unity-prototype-01/Assets/Scripts/FollowPlayer.cs
+++ unity-prototype-01/Assets/Scripts/FollowPlayer.cs
@@ -16,7 +16,7 @@ public class FollowPlayer : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         transform.position = player.transform.position + offset;
     }
