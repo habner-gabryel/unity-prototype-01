21516d013794f9c185bb4b1f66c56106bbb3559b
habner-gabryel <habner.gabryel.cc@gmail.com>
2024-07-09 18:13:44 -0300

    1.3.4 commit

--- unity-prototype-01/Assets/Scripts/FollowPlayer.cs
+++ unity-prototype-01/Assets/Scripts/FollowPlayer.cs
@@ -7,6 +7,8 @@ public class FollowPlayer : MonoBehaviour
 
     public GameObject player;
 
+    public Vector3 offset = new Vector3(0,5,-7);
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +18,6 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = player.transform.position;
+        transform.position = player.transform.position + offset;
     }
 }
