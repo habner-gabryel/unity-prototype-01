5bae55bc24f314f0d72b410b5b9f45d779441df5
habner-gabryel <habner.gabryel.cc@gmail.com>
2024-07-24 22:24:11 -0300

    6.2.1 commit

--- unity-prototype-01/Assets/Scripts/PlayerController.cs
+++ unity-prototype-01/Assets/Scripts/PlayerController.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float speed = 10.8F;
+    // [SerializeField] private float speed = 10.8F;
+    [SerializeField]private float horsePower = 0;
     [SerializeField] private float turnSpeed = 50;
     private float horizontalInput;
     private float forwardInput;
+    private Rigidbody playerRb;
+
+    void Start(){
+        playerRb = GetComponent<Rigidbody>();
+    }
 
     // Update is called once per frame
     void FixedUpdate()
@@ -15,8 +21,10 @@ public class PlayerController : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
 
+        playerRb.AddRelativeForce(Vector3.forward * horsePower * forwardInput);
+
         // Moves the vehicle forward based on vertical input
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         // Rotates the vehicle forward based on vertical input
         transform.Rotate(new Vector3(0, turnSpeed * horizontalInput * Time.deltaTime, 0));
     }
